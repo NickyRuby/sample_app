564532025d09957fe6e765310717b86cbcc52748
Nikita Lobyntsev <nikita.lobyntsev@gmail.com>
2015-11-22 18:50:37 +0200

    Добавлена регистрация пользователей

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20151121153620) do
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "pasword_digest"
     t.string   "password_digest"
   end
 
